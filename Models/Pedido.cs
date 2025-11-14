914240c441dbec20fe4bd4e1b9066432074a6cfe
luiz <luizaraujo308farias@gmail.com>
2025-11-14 10:40:53 -0300

    style: Esboço Tela Cardápio

---
+++ Models/Pedido.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sakura_Sushi.Models
+{
+    public class Pedido : Controller
+    {
+        int idPedido { get; set; }
+        DateOnly data { get; set;}
+        
+
+
+        enum StatusPedido
+        {
+            Pendente = "Pendente",
+
+        }
+    }
+}
